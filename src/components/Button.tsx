0fbbdb1bf31694d3b5650955d65ba721d2d55769
Wesley Silva <wesley@Wesleys-MacBook-Air.local>
2021-06-21 17:44:53 -0300

    feat: initialize firebase

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+type ButtonProps = {
+  children?: string;
+}
+
+export function Button(props: ButtonProps) {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    setCounter(counter + 1);
+  }
+
+  return (
+    <button onClick={increment}>
+      { props.children || counter }
+    </button>
+  );
+}
